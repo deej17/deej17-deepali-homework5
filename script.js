99b29b2b685b3a1fc2a43f6c71293bf391b378d2
Deepali Jain <61299175+deej17@users.noreply.github.com>
2020-04-07 13:50:30 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//To print the current date
+var date = moment().format("MMMM Do YYYY");
+$("#currentDay").text(date);
+
+
+
+
+
+var button = document.createElement("button");
+    button.textContent = "Complete";
+
+    li.appendChild(button);
+    todoList.appendChild(li);
